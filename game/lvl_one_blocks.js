bf754a71b1c7adc934018374147d38632a720202
Jeffrey Kedda <80783850+jeffke110@users.noreply.github.com>
2022-11-11 16:53:59 -0500

    Update lvl_one_blocks.js

--- game/lvl_one_blocks.js
+++ game/lvl_one_blocks.js
@@ -19,9 +19,9 @@ class block{
         
         image(lvl_one_block, this.x, this.y, 20, 20);
         //text(this.x + "-" + this.y, this.x - 10, this.y);
-        stroke('red'); // Change the color
-        strokeWeight(3); // Make the points 10 pixels in size
-        point(this.x, this.y);
+        //stroke('red'); // Change the color
+        //strokeWeight(3); // Make the points 10 pixels in size
+        //point(this.x, this.y);
     }
 }
 class ground{
@@ -35,12 +35,12 @@ class ground{
     draw(x, y) {
         //load character
         image(ground_img, x, y, 20);
-        if(this.right_ledge == true){
-            text(this.x + "-" + this.y, this.x - 10, this.y);
-        }
-        stroke('red'); // Change the color
-        strokeWeight(3); // Make the points 10 pixels in size
-        point(this.x, this.y + 10);
+        //if(this.right_ledge == true){
+        //    text(this.x + "-" + this.y, this.x - 10, this.y);
+        //}
+        //stroke('red'); // Change the color
+        //strokeWeight(3); // Make the points 10 pixels in size
+        p//oint(this.x, this.y + 10);
 
     }
 }
